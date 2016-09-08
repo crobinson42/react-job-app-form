942f94fc4101edd121596b165ed6681c7d1f3262
cory robinson <coryrobinson42@gmail.com>
2016-09-08 12:07:53 -0700

    fix(build): add .travis.yml

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+
+import babel from 'rollup-plugin-babel'
+import es2015Rollup from 'babel-preset-es2015-rollup'
+
+export default {
+  entry: 'src/index.js',
+  format: 'cjs',
+  plugins: [
+    babel({
+      'plugins': [
+        ['transform-react-jsx']
+      ],
+      'presets': [es2015Rollup]
+    })
+  ],
+  dest: 'dist/index.js'
+}
