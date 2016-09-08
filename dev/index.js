dd3c470d189479c5bc6cd4226114c8dfc733962f
cory robinson <coryrobinson42@gmail.com>
2016-09-08 13:30:12 -0700

    fix(ghpages): add dev build for gh-pages

--- dev/index.js
+++ dev/index.js
@@ -1,19 +1,9 @@
-'use strict';
+/**
+ * This is a DEV export for the Webpack dev server
+ */
 
-var _react = require('react');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from '../src/index'
 
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = require('react-dom');
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _index = require('../src/index');
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_reactDom2.default.render(_react2.default.createElement(_index2.default, null), document.querySelector('#app')); /**
-                                                                                                                  * This is a DEV export for the Webpack dev server
-                                                                                                                  */
+ReactDOM.render(<Component />, document.querySelector('#app'))
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   entry: './dev/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dev'),
     publicPath: '/assets/',
     filename: 'bundle.js'
   },
